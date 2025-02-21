9ebcbef389ea8f68cfa9b8471b3bbeea1b5eabf1
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-02-20 21:01:59 -0300

    training

---
+++ listas_extras/MainBradesco.py
@@ -0,0 +1,10 @@
+class MainBradesco:
+    pass
+
+print("Olá, a classe MainBradesco está funcionando!!")
+
+from Cliente import Cliente
+cliente1 = Cliente("Mariana", 98888)
+
+print (cliente1)
+print(cliente1.nome, cliente1.cpf)
