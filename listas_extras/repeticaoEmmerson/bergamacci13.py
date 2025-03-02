62382bf3cd513d75b55a689ad2f14fa937899916
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-03-02 18:25:47 -0300

    Resolving Ricci

--- listas_extras/repeticaoEmmerson/bergamacci13.py
+++ listas_extras/repeticaoEmmerson/bergamacci13.py
@@ -1,1 +1,10 @@
 '''13.Fazer um programa para achar a série de Bergamacci dos n primeiros termos: 1 1 1 1 3 5 9 17 ...'''
+
+qntd_termos = int(input("Digite a quantidade de termos que vc deseja ver na séria Bergamacci: "))
+n= 1
+aux = n
+soma_sequencia = 0
+
+print(f"A sequência Bergamacci com {qntd_termos} termos é:")
+for i in range(0, qntd_termos):
+    ouu
--- listas_extras/repeticaoEmmerson/ricci14.py
+++ listas_extras/repeticaoEmmerson/ricci14.py
@@ -1,3 +1,16 @@
 '''14.A série de Ricci difere da série de Fibonacci porque os dois primeiros termos podem ser
 definidos pelo usuário. Imprima os n primeiros termos da série de Ricci.'''
 
+qntd_termos = int(input("Digite a quantidade de termos para a série Ricci: "))
+termo_1 = int(input("Digite o primeiro termo da sua sequência: "))
+termo_2 = int(input("Digite o segundo termo da sua sequência: "))
+
+n = termo_1
+aux = termo_1
+diferenca_sequencia = termo_2-termo_1
+
+for i in range(0, qntd_termos):
+    print(n)
+    aux =n
+    n+=diferenca_sequencia
+    diferenca_sequencia = aux
