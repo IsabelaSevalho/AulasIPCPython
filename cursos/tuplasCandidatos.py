b3a3a0942f7fbea2ab2ed83f374f57db664fab0d
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-03-06 10:52:19 -0300

    Isn't working completely: Se errar o numero no cadastro do candidato, ele não registra; Erro na lista de chaves e valores

--- cursos/tuplasCandidatos.py
+++ cursos/tuplasCandidatos.py
@@ -12,4 +12,93 @@ confirmar. Números inválidos devem ser computados como votos nulos. Já o text
 deve ser contabilizado como voto em branco. A sumarização dos votos deve ser feita
 usando um dicionário. Ao término da votação, o simulador deve mostrar o total e a
 porcentagem de votos de cada candidato, nulos e brancos.'''
+import os
 
+dic_candidatos = {}
+dic_votos = {}
+global numero
+
+def validaNumero(n):
+    while not n.isdigit():
+        print("Número inválido, digite novamente!!")
+        n = input("\nDigite o número do candidato: ")
+        numero = n
+
+    else:
+        while len(n) != 2:
+            print("Número inválido, digite novamente! Só são aceitos números com 2 dígitos!")
+            n = input("\nDigite o número do candidato: ")
+        else:
+            numero = n
+
+
+def votacao():
+    global total
+    total = 0
+    continuar = True
+
+    while continuar:
+        n_voto = input("Digite o número do candidato que vc deseja votar: ")
+
+        if n_voto is None:
+            dic_votos["Votos brancos"] +=1
+            total += 1
+
+        elif (not n_voto.isdigit()) or len(n_voto)!=2 or n_voto not in dic_candidatos :
+            dic_votos["Votos nulos"] +=1
+            total += 1
+
+        else:
+            print("\n\nO seu candidato é: ", dic_candidatos[n_voto].upper(),
+                  "\nGostaria de confirmar a votação? Digite S para \"sim\" ou N para \"não\"")
+
+            if input().upper() == "S":
+                dic_votos[n_voto] += 1
+                total += 1
+                print("Votação confirmada!")
+                os.system('cls')
+
+            else:
+                print("Voto cancelado!")
+
+        sim_ou_nao = input("Deseja continuar? S para sim e N para não: ")
+        if sim_ou_nao.upper() == "S":
+            continuar = True
+        else:
+            continuar = False
+
+
+try:
+    qntd_candidatos = int(input("Digite a quantidade de candidatos que vc deseja cadastrar: "))
+
+    for i in range(qntd_candidatos):
+        numero = input("\nDigite o número do candidato: ")
+        validaNumero(numero)
+
+        nome = input("Digite o nome do candidato: ")
+
+        dic_candidatos[numero] = nome  # criação de um item em dicionário
+        dic_votos[numero] = 0
+
+    else:
+        dic_votos["Votos brancos"] = 0
+        dic_votos["Votos nulos"] = 0
+
+        os.system('cls')  # limpa a 'tela'/console
+        print("\n\nVOTAÇÃO VEEEI  :)\n")
+        votacao()
+
+        print("Os resultados da votação, com o total de", total, "votos, são:")
+        lista_voto = dic_votos.keys()
+        lista_qntd_voto = dic_votos.values()
+
+        for i in range(len(lista_voto)):
+            print(f"Para {lista_voto[i].upper()}: {lista_qntd_voto[i]} votos"
+                  f"\nPorcentagem: {(lista_qntd_voto[i] / 100) * total}%;\n")
+        else:
+            print("FIM :)")
+
+except ValueError:
+    print("Ocorreu um erro de digitação de algum valor! Tente novamente.")
+except Exception as e:
+    print("Ocorreu o erro: ", e)
