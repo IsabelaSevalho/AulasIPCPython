cf517e1dd299ec4d0abe815d5108d5baa2a2f2e0
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-03-11 14:25:25 -0300

    Class code

---
+++ exercicios_aula/strings1103.py
@@ -0,0 +1,5 @@
+frase = input("Digite uma frase: ")
+nome = input("Digite um nome: ")
+sobrenome = input("Digite um sobrenome: ")
+
+print(f"Frase: {frase}\nTamanho: {len(frase)}\nNome: {nome} e Sobrenome: {sobrenome}")
