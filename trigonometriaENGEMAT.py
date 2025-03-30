d6485fc2ee11e20b1f138d0691d4192f53a192a9
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-03-30 13:39:56 -0300

    Code for ENGEMAT with tan

--- trigonometriaENGEMAT.py
+++ trigonometriaENGEMAT.py
@@ -3,28 +3,41 @@ import matplotlib as plt #criar, alterar e configurar gráficos ++matplotlib.pyp
 
 #%matplotlib inline
 
-def plotUnitCircle(x, y):
-    eixos = plt.subplot() #produz figura em vários plots (áreas) diferentes, retorna os eixos x e y
-    eixos.grid(True) #grade para visualizar as posições
+def plotUnitCircle(x_cosseno, y_seno, tangente):
+    eixos = plt.subplot()  # produz figura em vários plots (áreas) diferentes, retorna os eixos x e y
+    eixos.grid(True)  # grade para visualizar as posições
 
-    #CÍRCULO TRIGONOMÉTRICO
-    circulo = plt.Circle((0,0), radius=1.0, edgecolor = "black", facecolor = "none")
+    # CÍRCULO TRIGONOMÉTRICO
+    circulo = plt.Circle((0, 0), radius=1.0, edgecolor="black", facecolor="none")
     eixos.add_artist(circulo)
 
-    #PLANO CARTESIANO
-    eixos.axhline(y=0.0, color="black") #insere linha horizontal que define o eixo x
-    eixos.axvline(x=0.0, color="black") #define o eixo y
+    # PLANO CARTESIANO
+    eixos.axhline(y=0.0, color="black")  # insere linha horizontal que define o eixo x
+    eixos.axvline(x=0.0, color="black")  # define o eixo y
 
-    plt.xlim(-1.0, 1.0) #define o limite de x no plot
-    plt.ylim(-1.0, 1.0) #define o limite de x no plot
+    # verificação para que não corte a tangente ao MONTAR O PLANO CARTESIANO
+    if tangente <= 1:
+        plt.ylim(-1.0, 1.0)  # define o limite de y no plot
+        plt.xlim(-1.0, 1.0)  # define o limite de x no plot
 
-    #TRIANGULO
-    eixos.plot([0,x],[0,0], color ="blue", label="cateto adjacente") #define a posicao x e y da linha, inicial e final
-    eixos.plot([x,x],[0,y], color ="green", label="cateto oposto")
-    eixos.plot([0,x],[0,y], color ="red", label="hipotenusa")
+    else:
+        plt.ylim(-(round(tangente, 0) + 0.5), (round(tangente, 0) + 0.5))  # limite de y
+        plt.xlim(-(round(tangente, 0) + 0.5), (round(tangente, 0) + 0.5))  # limite de y
 
-    #EXIBIÇÃO
-    plt.legend() #mostra os rótulos escritos (ex: cat op, cat adj, hipotenusa)
+    # TRIANGULO
+    eixos.plot([0, x_cosseno], [0, 0], color="blue",
+               label="cateto adjacente")  # define a posicao x e y da linha, inicial e final
+    eixos.plot([x_cosseno, x_cosseno], [0, y_seno], color="green", label="cateto oposto")
+    eixos.plot([0, x_cosseno], [0, y_seno], color="red", label="hipotenusa")
+
+    if x_cosseno > 0:
+        eixos.plot([1.0, 1.0], [0, tangente], color="yellow", label="tangente")
+    elif x_cosseno < 0:
+        eixos.plot([-1.0, -1.0], [0, tangente], color="yellow", label="tangente")
+        # caso cosseno igual a 0, tangente não existe, pois tangente = seno/cosseno
+
+    # EXIBIÇÃO
+    plt.legend()  # mostra os rótulos escritos (ex: cat op, cat adj, hipotenusa)
     plt.title("Nome do Gráfico")
     plt.show()
 
@@ -33,11 +46,13 @@ angulo = float(input("Digite um valor para o ângulo em graus: "))
 angulo = math.radians(angulo)
 
 #cos t = x (base, adjacente)-----cos t = adj/hipo
-x= math.cos(angulo)
+x_cosseno= math.cos(angulo)
 
 # sen t = y (altura do triangulo, oposto)----sen t = op/hipo
-y= math.sin(angulo)
+y_seno= math.sin(angulo)
 
-print("Cosseno do ângulo (x) =", round(x, 2),"\nSeno do ângulo (y) =", round(y, 2))
+tangente = math.tan(angulo)
 
-plotUnitCircle(x, y)
+print("Cosseno do ângulo (x) =", round(x_cosseno, 2),"\nSeno do ângulo (y) =", round(y_seno, 2), "\nTangente do ângulo =", round(tangente, 2))
+
+plotUnitCircle(x_cosseno, y_seno, tangente)
