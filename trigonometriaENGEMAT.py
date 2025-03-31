7a27efdb1304bc6d8d7a761110156ff1f3e26c9f
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-03-31 08:39:20 -0300

    Adding final ENGEMAT code

--- trigonometriaENGEMAT.py
+++ trigonometriaENGEMAT.py
@@ -4,59 +4,57 @@ import matplotlib as plt #criar, alterar e configurar gráficos ++matplotlib.pyp
 #%matplotlib inline
 
 def plotUnitCircle(x_cosseno, y_seno, tangente):
-    eixos = plt.subplot()  # produz figura em vários plots (áreas) diferentes, retorna os eixos x e y
-    eixos.grid(True)  # grade para visualizar as posições
+    eixos = plt.subplot() #produz figura em vários plots (áreas) diferentes, retorna os eixos x e y
+    eixos.grid(True) #grade para visualizar as posições
 
-    # CÍRCULO TRIGONOMÉTRICO
-    circulo = plt.Circle((0, 0), radius=1.0, edgecolor="black", facecolor="none")
+    #CÍRCULO TRIGONOMÉTRICO
+    circulo = plt.Circle((0,0), radius=1.0, edgecolor = "black", facecolor = "none")
     eixos.add_artist(circulo)
 
-    # PLANO CARTESIANO
-    eixos.axhline(y=0.0, color="black")  # insere linha horizontal que define o eixo x
-    eixos.axvline(x=0.0, color="black")  # define o eixo y
 
-    # verificação para que não corte a tangente ao MONTAR O PLANO CARTESIANO
-    if (tangente > 0.0 and tangente <= 1.0) or (tangente < 0.0 and tangente >= (-1.0)):
-        plt.ylim(-1.25, 1.25)  # define o limite de y no plot
-        plt.xlim(-1.25, 1.25)  # define o limite de x no plot
+    #PLANO CARTESIANO
+    eixos.axhline(y=0.0, color="black") #insere linha horizontal que define o eixo x
+    eixos.axvline(x=0.0, color="black") #define o eixo y
 
-    elif tangente > 1.0 or tangente < (-1.0):
-        plt.ylim(-(round(tangente, 0) + 0.25), (round(tangente, 0) + 0.25))  # define o limite de y no plot
-        plt.xlim(-(round(tangente, 0) + 0.25), (round(tangente, 0) + 0.25))  # define o limite de x no plot
+    max_limit = max(abs(tangente), 1.0) + 0.2  # Limite baseado na tangente ou 1.0
+    plt.xlim(-max_limit, max_limit) #define o limite de x no plot
+    plt.ylim(-max_limit, max_limit) #define o limite de y no plot
 
-    # TRIANGULO
-    eixos.plot([0.0, x_cosseno], [0.0, 0.0], color="blue",
-               label="cateto adjacente")  # define a posicao x e y da linha, inicial e final
-    eixos.plot([x_cosseno, x_cosseno], [0.0, y_seno], color="green", label="cateto oposto")
-    eixos.plot([0.0, x_cosseno], [0.0, y_seno], color="red", label="hipotenusa")
 
-    if x_cosseno < 0.0:
-        eixos.plot([-1.0, -1.0], [0.0, tangente], color="orange", label="tangente")
+    #TRIANGULO
+    eixos.plot([0.0,x_cosseno],[0.0,0.0], color ="blue", label="cateto adjacente") #define a posicao x e y da linha, inicial e final
+    eixos.plot([x_cosseno,x_cosseno],[0.0,y_seno], color ="green", label="cateto oposto")
+    eixos.plot([0.0,x_cosseno],[0.0,y_seno], color ="red", label="hipotenusa")
 
-    elif x_cosseno > 0.0:
-        eixos.plot([1.0, 1.0], [0.0, tangente], color="orange", label="tangente")
-        # caso cosseno igual a 0, tangente não existe, pois tangente = seno/cosseno
+    if x_cosseno<0.0:
+      tangente = -tangente
+      eixos.plot([-1.0, -1.0], [0.0, tangente], color="orange", label="tangente")
+      eixos.plot([x_cosseno, -1.0],[y_seno, tangente], linestyle='dashed', color ="grey") #reta TRACEJADA para indicar a extensão para o encontro da tangente
 
-    eixos.plot([x_cosseno, x_cosseno], [y_seno, y_seno], "o",
-               color="purple")  # ponto único, "o" define que vai ter formato de ponto
-    eixos.plot()  # reta para indicar a extensão para o encontro da tangente
+    elif x_cosseno>0.0:
+      eixos.plot([1.0, 1.0], [0.0, tangente], color="orange", label="tangente")
+      eixos.plot([x_cosseno, 1.0],[y_seno, tangente], linestyle='dashed', color ="grey") #reta TRACEJADA para indicar a extensão para o encontro da tangente
 
-    # EXIBIÇÃO
-    plt.legend()  # mostra os rótulos escritos (ex: cat op, cat adj, hipotenusa)
+    #caso cosseno igual a 0, tangente não existe, pois tangente = seno/cosseno, tangente= oposto/adjacente
+
+    eixos.plot([x_cosseno,x_cosseno], [y_seno, y_seno], "o", color="purple") #ponto único, "o" define que vai ter formato de ponto
+
+    #EXIBIÇÃO
+    plt.legend() #mostra os rótulos escritos (ex: cat op, cat adj, hipotenusa)
     plt.title("Trigonometria com Python")
     plt.show()
 
-#CÓDIGO FORA FUNÇÃO
+#FORA FUNÇÃO
 angulo = float(input("Digite um valor para o ângulo em graus: "))
-angulo = math.radians(angulo)
+angulo_r = math.radians(angulo)
 
 #cos t = x (base, adjacente)-----cos t = adj/hipo
-x_cosseno= math.cos(angulo)
+x_cosseno= math.cos(angulo_r)
 
 # sen t = y (altura do triangulo, oposto)----sen t = op/hipo
-y_seno= math.sin(angulo)
+y_seno= math.sin(angulo_r)
 
-tangente = math.tan(angulo)
+tangente = math.tan(angulo_r)
 
 print("Cosseno do ângulo (x) =", round(x_cosseno, 2),"\nSeno do ângulo (y) =", round(y_seno, 2), "\nTangente do ângulo =", round(tangente, 2))
 
